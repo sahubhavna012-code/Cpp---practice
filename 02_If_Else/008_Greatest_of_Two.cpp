7e07f64b139622ddb6f9bd398f1bfe6930c40d95
Bhavna Sahu <sahubhavna012@gmail.com>
2026-08-21 21:21:40 +0530

    add

---
+++ 02_If_Else/008_Greatest_of_Two.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<a<<" is greater than "<<b;
+    }
+    else
+    {
+        cout<<b<<" is greater than "<<a;
+    }
+}
